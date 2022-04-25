aa0e8303f595c929cbd98be623b289c60af47a6e
CodewaveSandy <sandy@codewave.com>
2022-04-25 20:40:45 +0530

    git commands added

--- bin/cli.js
+++ bin/cli.js
@@ -14,7 +14,7 @@ const runCommand = (command) => {
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/codewave-new/create-cwr-project ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
-const gitInitializeCommand = `cd ${repoName} && git init`;
+const gitInitializeCommand = `cd ${repoName} && git init && git remote rm origin`;
 
 console.log(`Cloning the repository with name ${repoName}`);
 const checkedOut = runCommand(gitCheckoutCommand);
@@ -24,12 +24,23 @@ console.log(`Installing dependencies for ${repoName}`);
 const installedDeps = runCommand(installDepsCommand);
 if (!installedDeps) process.exit(-1);
 
+console.log('--------------------------------------------');
 console.log('Successfully Installed all dependecies.');
+console.log('--------------------------------------------');
+
+console.log('Setting up GIT for first time usage...');
 
 const gitInitialize = runCommand(gitInitializeCommand);
 if (!gitInitialize) process.exit(-1);
 
+console.log('*************************************************');
+console.log('GIT setup complete');
+console.log('*************************************************');
+
+console.log('--------------------------------------------');
 console.log(
   'Congratulations! You are ready. Follow the following commands to start'
 );
+console.log('--------------------------------------------');
+
 console.log(`cd ${repoName} && npm start`);
