be704372c94661d897f9ce7224ef161b43a52c65
CodewaveSandy <sandy@codewave.com>
2022-04-28 17:29:30 +0530

    Responsive CSS added with color output in console

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 const { execSync } = require('child_process');
 
+const cyan = '\u001b[1m\u001b[36;1m';
+const reset = '\u001b[0m';
+const red = '\u001b[1m\u001b[31;1m';
+const yellow = '\u001b[1m\u001b[33;1m';
+const blue = '\u001b[1m\u001b[34;1m';
+
 const runCommand = (command) => {
   try {
     execSync(`${command}`, { stdio: 'inherit' });
   } catch (e) {
-    console.error(`Failed to execute ${command}`, e);
+    console.error(`${red}Failed to execute ${command}${reset}`, e);
     return false;
   }
   return true;
@@ -16,31 +22,26 @@ const gitCheckoutCommand = `git clone --depth 1 https://github.com/codewave-new/
 const installDepsCommand = `cd ${repoName} && npm install`;
 const gitInitializeCommand = `cd ${repoName} && rm -r -f .git && git init`;
 
-console.log(`Cloning the repository with name ${repoName}`);
+console.log(`Cloning the repository with name ${blue}${repoName}${reset}\n\n`);
 const checkedOut = runCommand(gitCheckoutCommand);
 if (!checkedOut) process.exit(-1);
 
-console.log(`Installing dependencies for ${repoName}`);
+console.log(`Installing dependencies for ${yellow}${repoName}${reset}\n\n`);
 const installedDeps = runCommand(installDepsCommand);
 if (!installedDeps) process.exit(-1);
 
-console.log('--------------------------------------------');
-console.log('Successfully Installed all dependecies.');
-console.log('--------------------------------------------');
+console.log(`${yellow}Successfully Installed all dependecies.${reset}\n\n`);
 
-console.log('Setting up GIT for first time usage...');
+console.log('Setting up GIT for first time usage...\n\n');
 
 const gitInitialize = runCommand(gitInitializeCommand);
 if (!gitInitialize) process.exit(-1);
 
-console.log('*************************************************');
-console.log('GIT setup complete');
-console.log('*************************************************');
+console.log('GIT setup complete\n\n');
 
-console.log('--------------------------------------------');
 console.log(
-  'Congratulations! You are ready. Follow the following commands to start'
+  `${cyan}Congratulations! You are ready. Follow the following commands to start${reset}\n\n`
 );
-console.log('--------------------------------------------');
 
-console.log(`cd ${repoName} && npm start`);
+console.log(`${cyan}cd${reset} ${repoName}\n\n`);
+console.log(`${cyan}npm${reset} start\n\n`);
